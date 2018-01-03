1734123b678fcc6df8e239fdb97327a7f449ba0c
AdrianPiechowicz <rarowul@gmail.com>
2018-01-03 13:35:18 +0100

    First Commit

--- src/main/java/com/kodilla/Calculator.java
+++ src/main/java/com/kodilla/Calculator.java
@@ -1,4 +1,24 @@
 package com.kodilla;
 
 public class Calculator {
+
+    public void getStarted(){
+        System.out.println("Hello, this is a simple calculator! :)");
+    }
+
+    public int sum(int a, int b){
+        return a+b;
+    }
+
+    public int subtraction(int a,int b){
+        return a-b;
+    }
+
+    public static void main(String args[]){
+        Calculator kalk = new Calculator();
+        kalk.getStarted();
+        int a = 5;
+        int b=2;
+        System.out.println("Two selected numbers are "+a+" and "+b+ " sum of those equals "+kalk.sum(a,b)+" subtraction equals "+kalk.subtraction(a,b));
+    }
 }
